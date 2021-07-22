d91dbff8bab56476327a59c0a5a5f5a4f6c9986a
julioribeiiro <jmoraes@daitan.com>
2021-07-22 16:33:03 -0300

    project created

---
+++ backend/src/main/java/com/julioapps/sellingsapp/SellingsappApplication.java
@@ -0,0 +1,13 @@
+package com.julioapps.sellingsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellingsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellingsappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/julioapps/sellingsapp/SellingsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioapps.sellingsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellingsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
